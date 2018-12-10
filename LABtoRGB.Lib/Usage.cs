078748a913e3708e496728970d3208622b261ade
이정규 <i6020345@live.co.kr>
2018-12-10 14:31:52 +0900

    add Colourful library

---
+++ LABtoRGB.Lib/Usage.cs
@@ -0,0 +1,29 @@
+﻿using Colourful;
+using Colourful.Conversion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LABtoRGB.Lib
+{
+    public class Usage
+    {
+        private void RGBtoXYZ()
+        {
+            RGBColor input = new RGBColor(1, 0, 0);
+
+            var converter = new ColourfulConverter { WhitePoint = Illuminants.D65 };
+
+            XYZColor output = converter.ToXYZ(input);
+        }
+
+        private void LABColor()
+        {
+            LabColor input = new LabColor(10, 20, 30, Illuminants.D50);
+
+            var converter = new ColourfulConverter { TargetLabWhitePoint = Illuminants.D65 };
+
+            LabColor output = converter.Adapt(input);
+        }
+    }
+}

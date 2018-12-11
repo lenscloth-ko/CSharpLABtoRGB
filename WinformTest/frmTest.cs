679aa922bfe8978b3a5949555bb8f8ad1dbd93b9
이정규 <i6020345@live.co.kr>
2018-12-11 19:19:37 +0900

    test project 추가

---
+++ WinformTest/frmTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinformTest
+{
+    public partial class frmTest : Form
+    {
+        public frmTest()
+        {
+            InitializeComponent();
+        }
+    }
+}
